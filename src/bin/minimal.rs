91dab01ff6acba54d551fc93b37cae17f1c86a20
Devin Droddy <devin.droddy@gmail.com>
2024-01-11 23:25:22 -0500

    initialize working RTIC template

---
+++ src/bin/minimal.rs
@@ -0,0 +1,63 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+
+use robot_tour_robot_code as _; // global logger + panicking-behavior + memory layout
+
+// TODO(7) Configure the `rtic::app` macro
+#[rtic::app(
+    device = stm32f4xx_hal::pac,
+    // TODO: Replace the `FreeInterrupt1, ...` with free interrupt vectors if software tasks are used
+    // You can usually find the names of the interrupt vectors in the some_hal::pac::interrupt enum.
+    dispatchers = [USART1]
+)]
+mod app {
+    // Shared resources go here
+    #[shared]
+    struct Shared {
+        // TODO: Add resources
+    }
+
+    // Local resources go here
+    #[local]
+    struct Local {
+        // TODO: Add resources
+    }
+
+    #[init]
+    fn init(_cx: init::Context) -> (Shared, Local) {
+        defmt::info!("init");
+
+        // TODO setup monotonic if used
+        // let sysclk = { /* clock setup + returning sysclk as an u32 */ };
+        // let token = rtic_monotonics::create_systick_token!();
+        // rtic_monotonics::systick::Systick::new(cx.core.SYST, sysclk, token);
+
+        task1::spawn().ok();
+
+        (
+            Shared {
+                // Initialization of shared resources go here
+            },
+            Local {
+                // Initialization of local resources go here
+            },
+        )
+    }
+
+    // Optional idle, can be removed if not needed.
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        defmt::info!("idle");
+
+        loop {
+            continue;
+        }
+    }
+
+    // TODO: Add tasks
+    #[task(priority = 1)]
+    async fn task1(_cx: task1::Context) {
+        defmt::info!("Hello from task1!");
+    }
+}
